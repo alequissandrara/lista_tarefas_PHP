c9125772887b5571cd5ff2f56b42a589eba83689
alequissandrara <alquissandrarayane123@gmail.com>
2023-06-29 17:19:50 -0300

    inserindo controles de confirmação de nova tarefa  criada

--- tarefa_controller.php
+++ tarefa_controller.php
@@ -3,21 +3,21 @@
     require '../app_lista_tarefas/tarefa.model.php';
     require '../app_lista_tarefas/tarefa.service.php';
     require '../app_lista_tarefas/conexao.php';
+   
+    $acao = isset($_GET['acao']) ? $_GET['acao'] == 'inserir' : $acao;
 
-    echo '<pre>';
-    print_r($_POST);
-    echo '</pre>';
+    if($acao == 'inserir' ) {
+        $tarefa = new Tarefa();
+        $tarefa->__set('tarefa', $_POST['tarefa']);
 
-    $tarefa = new Tarefa();
-    $tarefa->__set('tarefa', $_POST['tarefa']);
+        $conexao = new Conexao();
+        
+        $tarefaService = new TarefaService($conexao, $tarefa);
+        $tarefaService->inserir();
 
-    $conexao = new Conexao();
-    
-    $tarefaService = new TarefaService($conexao, $tarefa);
-    $tarefaService->inserir();
-
-    echo '<pre>';
-    print_r($tarefaService);
-    echo '</pre>';
+        header('location: nova_tarefa.php?inclusao=1');
+    }else if($acao == 'recuperar') {
+        echo 'Chegamos até aqui';
+    }
 
 ?>

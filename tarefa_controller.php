d3b27c18336c2328c49bcf6509c89cf598ee9ea0
alequissandrara <alquissandrarayane123@gmail.com>
2023-07-11 15:06:25 -0300

    Atualizando registros parte 2

--- tarefa_controller.php
+++ tarefa_controller.php
@@ -1,28 +1,44 @@
 <?php
 
-    require '../app_lista_tarefas/tarefa.model.php';
-    require '../app_lista_tarefas/tarefa.service.php';
-    require '../app_lista_tarefas/conexao.php';
-   
-    $acao = isset($_GET['acao']) ? $_GET['acao'] == 'inserir' : $acao;
+	require "../app_lista_tarefas/tarefa.model.php";
+	require "../app_lista_tarefas/tarefa.service.php";
+	require "../app_lista_tarefas/conexao.php";
 
-    if($acao == 'inserir' ) {
-        $tarefa = new Tarefa();
-        $tarefa->__set('tarefa', $_POST['tarefa']);
 
-        $conexao = new Conexao();
-        
-        $tarefaService = new TarefaService($conexao, $tarefa);
-        $tarefaService->inserir();
+	$acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
 
-        header('location: nova_tarefa.php?inclusao=1');
-    }else if($acao == 'recuperar') {
+	if($acao == 'inserir' ) {
+		$tarefa = new Tarefa();
+		$tarefa->__set('tarefa', $_POST['tarefa']);
 
-       $tarefa = new Tarefa();
-       $conexao = new Conexao();
+		$conexao = new Conexao();
 
-       $tarefaService = new TarefaService($conexao, $tarefa);
-       $tarefas = $tarefaService->recuperar();
-    }
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefaService->inserir();
+
+		header('Location: nova_tarefa.php?inclusao=1');
+	
+	} else if($acao == 'recuperar') {
+		
+		$tarefa = new Tarefa();
+		$conexao = new Conexao();
+
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefas = $tarefaService->recuperar();
+	
+	} else if($acao == 'atualizar') {
+
+		$tarefa = new Tarefa();
+		$tarefa->__set('id', $_POST['id']);
+		$tarefa->__set('tarefa', $_POST['tarefa']);
+
+		$conexao = new Conexao();
+
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		if($tarefaService->atualizar()) {
+			header('location: todas_tarefas.php');
+		}
+    
+	}
 
 ?>

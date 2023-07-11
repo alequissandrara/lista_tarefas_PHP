c53bab6469b9f7c25a61f37abfc3f10ef288faf4
alequissandrara <alquissandrarayane123@gmail.com>
2023-07-11 15:06:22 -0300

    Atualizando registros parte 2

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -1,6 +1,14 @@
 <?php
+
 	$acao = 'recuperar';
 	require 'tarefa_controller.php';
+
+	/*
+	echo '<pre>';
+	print_r($tarefas);
+	echo '</pre>';
+	*/
+
 ?>
 
 <html>
@@ -12,57 +20,58 @@
 		<link rel="stylesheet" href="css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-	<script>
-		function editar(id, txt_tarefa){
-			//criar um form de edição
-			let form = document.createElement('form')
-			form.action ='#'
-			form.method = 'post'
-			form.className = 'row'
 
-			//criar um input para entrada do texto
-			let inputTarefa = document.createElement('input')
-			inputTarefa.type ='text'
-			inputTarefa.name ='tarefa'
-			inputTarefa.className = 'col-9 form-control'
-			inputTarefa.value = txt_tarefa
+		<script>
+			function editar(id, txt_tarefa) {
 
-			//criar um input hidden para guardar o id da tarefa
-			let inputId = document.createElement('input')
-			inputId.type = 'hidden'
-			inputId.name = 'id'
-			inputId.value = id
+				//criar um form de edição
+				let form = document.createElement('form')
+				form.action = 'tarefa_controller.php?acao=atualizar'
+				form.method = 'post'
+				form.className = 'row'
 
+				//criar um input para entrada do texto
+				let inputTarefa = document.createElement('input')
+				inputTarefa.type = 'text'
+				inputTarefa.name = 'tarefa'
+				inputTarefa.className = 'col-9 form-control'
+				inputTarefa.value = txt_tarefa
 
-			//criar um button para envio do form
-			let button = document.createElement('button')
-			button.type = 'submit'
-			button.className = 'col-3 btn btn-info'
-			button.innerHTML = 'Atualizar'
+				//criar um input hidden para guardar o id da tarefa
+				let inputId = document.createElement('input')
+				inputId.type = 'hidden'
+				inputId.name = 'id'
+				inputId.value = id
 
-			//incluir inputTarefa no form
-			form.appendChild(inputTarefa)
+				//criar um button para envio do form
+				let button = document.createElement('button')
+				button.type = 'submit'
+				button.className = 'col-3 btn btn-info'
+				button.innerHTML = 'Atualizar'
 
-			//incluir inputId no form
-			form.appendChild(inputId)
+				//incluir inputTarefa no form
+				form.appendChild(inputTarefa)
 
-			//incluir button no form
-			form.appendChild(button)
+				//incluir inputId no form
+				form.appendChild(inputId)
 
-			//teste
-			//console.log(form)
+				//incluir button no form
+				form.appendChild(button)
 
-			//selecionar a div tarefa
-			let tarefa = document.getElementById('tarefa_'+id)
+				//teste
+				//console.log(form)
 
-			//limpar o texto da tarefa para a incusão do form
-			tarefa.innerHTML = ''
+				//selecionar a div tarefa
+				let tarefa = document.getElementById('tarefa_'+id)
 
-			//incluir form na página
-			tarefa.insertBefore(form, tarefa[0])
-			
-		}
-	</script>
+				//limpar o texto da tarefa para inclusão do form
+				tarefa.innerHTML = ''
+
+				//incluir form na página
+				tarefa.insertBefore(form, tarefa[0])
+
+			}
+		</script>
 	</head>
 
 	<body>
@@ -95,7 +104,7 @@
 								<?php foreach($tarefas as $indice => $tarefa) { ?>
 									<div class="row mb-3 d-flex align-items-center tarefa">
 										<div class="col-sm-9" id="tarefa_<?= $tarefa->id ?>">
-											<?= $tarefa->tarefa ?>(<?= $tarefa->status ?>) 
+											<?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)
 										</div>
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
 											<i class="fas fa-trash-alt fa-lg text-danger"></i>
@@ -103,7 +112,9 @@
 											<i class="fas fa-check-square fa-lg text-success"></i>
 										</div>
 									</div>
+
 								<?php } ?>
+								
 							</div>
 						</div>
 					</div>

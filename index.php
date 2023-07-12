09af6bec90c268f5977c17b45f1cd0e9c3854a62
alequissandrara <alquissandrarayane123@gmail.com>
2023-07-12 16:17:41 -0300

    Listando tarefas pendentes

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+
+	$acao = 'recuperar';
+	require 'tarefa_controller.php';
+	
+	/*
+	echo '<pre>';
+	print_r($tarefas);
+	echo '</pre>';
+	*/
+?>
 <html>
 	<head>
 		<meta charset="utf-8" />
@@ -5,8 +16,69 @@
 		<title>App Lista Tarefas</title>
 
 		<link rel="stylesheet" href="css/estilo.css">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+	
+		<script>
+			function editar(id, txt_tarefa) {
+
+				//criar um form de edição
+				let form = document.createElement('form')
+				form.action = 'index.php?pag=index&acao=atualizar'
+				form.method = 'post'
+				form.className = 'row'
+
+				//criar um input para entrada do texto
+				let inputTarefa = document.createElement('input')
+				inputTarefa.type = 'text'
+				inputTarefa.name = 'tarefa'
+				inputTarefa.className = 'col-9 form-control'
+				inputTarefa.value = txt_tarefa
+
+				//criar um input hidden para guardar o id da tarefa
+				let inputId = document.createElement('input')
+				inputId.type = 'hidden'
+				inputId.name = 'id'
+				inputId.value = id
+
+				//criar um button para envio do form
+				let button = document.createElement('button')
+				button.type = 'submit'
+				button.className = 'col-3 btn btn-info'
+				button.innerHTML = 'Atualizar'
+
+				//incluir inputTarefa no form
+				form.appendChild(inputTarefa)
+
+				//incluir inputId no form
+				form.appendChild(inputId)
+
+				//incluir button no form
+				form.appendChild(button)
+
+				//teste
+				//console.log(form)
+
+				//selecionar a div tarefa
+				let tarefa = document.getElementById('tarefa_'+id)
+
+				//limpar o texto da tarefa para inclusão do form
+				tarefa.innerHTML = ''
+
+				//incluir form na página
+				tarefa.insertBefore(form, tarefa[0])
+
+			}
+
+			function remover(id) {
+				location.href = 'index.php?pag=index&acao=remover&id='+id;
+			}
+
+			function marcarRealizada(id){
+				location.href = 'index.php?pag=index&acao=marcarRealizada&id='+id;
+
+			}
+		</script>
 	</head>
 
 	<body>
@@ -36,28 +108,48 @@
 								<h4>Tarefas pendentes</h4>
 								<hr />
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Lavar o carro</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
-									</div>
-								</div>
+								<?php foreach($tarefas as $indice => $tarefa) { ?>
+									<?php if($tarefa->status == 'pendente') { ?>
+										<div class="row mb-3 d-flex align-items-center tarefa">
+											<div class="col-sm-9" id="tarefa_<?= $tarefa->id ?>">
+												<?= $tarefa->tarefa ?> 
+											</div>
+											<div class="col-sm-3 mt-2 d-flex justify-content-between">
+												<i class="fas fa-trash-alt fa-lg text-danger" style="cursor:pointer;" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter"></i>
+												
+												
+													<i class="fas fa-edit fa-lg text-info" style="cursor:pointer;" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
+													<i class="fas fa-check-square fa-lg text-success" style="cursor:pointer;" onclick="marcarRealizada(<?= $tarefa->id ?>)"></i>
+												
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Passear com o cachorro</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
-									</div>
-								</div>
+											</div>
+										</div>
+										<!-- Modal Diálogo -->
+										<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+											<div class="modal-dialog modal-dialog-centered" role="document">
+												<div class="modal-content">
+												<div class="modal-header">
+													<h5 class="modal-title" id="exampleModalLongTitle">Deseja excluir tarefa permanentemente?</h5>
+													<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+													<span aria-hidden="true">&times;</span>
+													</button>
+												</div>
+												<div class="modal-footer">
+													<button type="button" class="btn btn-primary" onclick="remover(<?= $tarefa->id ?>)">Sim, desejo!</button>
+													<button type="button" class="btn btn-secondary" data-dismiss="modal">Não, talvez depois </button>
+												</div>
+												</div>
+											</div>
+										</div>
+									<?php } ?>
+								<?php } ?>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
+		<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 	</body>
 </html>
